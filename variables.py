984f3b0e7adc55fe5f558255392ed0438fe5c229
Rohan Yadav <yadavrohan98040@gmail.com>
2025-09-17 23:38:37 +0530

    add:variables and datatypes in python related code

---
+++ variables.py
@@ -0,0 +1,19 @@
+# Variables and Data Types in Python
+
+# String
+name = "Rohan"
+
+# Integer
+age = 20
+
+# Float
+height = 5.9
+
+# Boolean
+is_student = True
+
+# Printing variables
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student:", is_student)
